f6438fec0f3e4f3be7b9f9e9de35bb7c7d91b0f6
vikmaamaa <maamaavictor@yahoo.com>
2023-03-08 09:14:55 +0100

    feat: installing and using pdfkit

--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Profile
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io 
 # Create your views here.
 
 
@@ -23,4 +27,16 @@ def accept(request):
 
 def resume(request, id):
     user_profile = Profile.objects.get(pk=id)
-    return render(request,'pdf/resume.html', {'user_profile': user_profile})
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile': user_profile})
+    options =  {
+        'page-size':'Letter',
+        'encoding': 'UTF-8'
+    }
+    pdf  = pdfkit.from_string(html, False, options)
+    response = HttpResponse(pdf, content_type='appliction/pdf')
+    response['Content-Disposition'] = 'attachment'
+    filename = "resume.pdf"
+    
+    return response 
+# render(request,'pdf/resume.html', {'user_profile': user_profile})
